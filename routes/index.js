da1ec2d16d3f405de3cbbc009bf0722ce829d484
samson-corbiere <samson.corbiere@gmail.com>
2021-02-03 21:06:32 +0100

    add route searches by name of an anime

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const animes = require('./animes');
+const searches = require('./searches');
 
 // eslint-disable-next-line
 module.exports = (app) => {
   app.use('/animes', animes);
-
+  app.use('/searches', searches)
 };
