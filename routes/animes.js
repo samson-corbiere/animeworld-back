dbd1952c49eee8d3909dfe999899e8f7f1dfd99d
samson-corbiere <samson.corbiere@gmail.com>
2021-02-04 10:28:28 +0100

    add new route novelty

--- routes/animes.js
+++ routes/animes.js
@@ -33,6 +33,22 @@ router.get("/view", async (req, res) => {
 }
 });
 
+//find anime ordered by novelty
+router.get("/novelty", async (req, res) => {
+    try {
+        const result = await connection.query(
+            `SELECT * FROM anime
+            ORDER BY start_date DESC`
+    );
+    res.status(200).json({
+        result
+    })
+} catch(err) {
+    console.log(err);
+    res.status(500).send("error get animes")
+}
+});
+
 //find anime with action genre
 router.get("/actionGenre", async (req, res) => {
     try {
