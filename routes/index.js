53dea59c93da64b55e998925f978144f327895e7
samson-corbiere <samson.corbiere@gmail.com>
2021-02-04 12:54:37 +0100

    routee pageAnime done

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const animes = require('./animes');
 const searches = require('./searches');
-const actorAnime = require('./actorAnime')
+const actorAnime = require('./actorAnime');
+const pageAnime = require('./pageAnime');
 
 // eslint-disable-next-line
 module.exports = (app) => {
   app.use('/animes', animes);
   app.use('/searches', searches);
   app.use('/actorAnime', actorAnime);
+  app.use('/pageAnime', pageAnime)
 };
